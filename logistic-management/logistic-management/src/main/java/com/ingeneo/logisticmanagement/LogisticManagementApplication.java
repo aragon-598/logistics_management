403eb22a3a03405298df9e72b339543311e04c32
Alejandro Aragon <aragondru155@gmail.com>
2023-10-14 08:29:58 -0600

    initial commit

---
+++ logistic-management/logistic-management/src/main/java/com/ingeneo/logisticmanagement/LogisticManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ingeneo.logisticmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticManagementApplication.class, args);
+	}
+
+}
---
+++ logistic-management/logistic-management/src/test/java/com/ingeneo/logisticmanagement/LogisticManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingeneo.logisticmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
